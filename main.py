483c935f85d17a9f46c0912f56bb79c1ce2603ac
Lakshay <lakshayd2002@gmail.com>
2025-04-22 22:35:52 -0400

    Changed output format to JSON rather than it showing up on cli, working on chunking and embedding

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import pdfplumber # Able to read pdf files and extract text
 import magic # Able to get the type of file
+import json
+import requests
+from langchain_text_splitters import RecursiveJsonSplitter
 
 # Assign directory
 directory = r"E:\Winter Sem\LocalRAG\Data"
@@ -18,8 +21,26 @@ for paths, folders, files in os.walk(directory):
             for page in pages:
                 # Extract text from each page
                 text = page.extract_text()
-                print(f"filename: {filename},\n"
-                      f"page: {page},\n"
-                      f"text:\n {text},\n")
-                      # f"type: {typeoffile}")
-                print()
+
+                # print(f"filename: {filename},\n"
+                #       f"page: {page},\n"
+                #       f"text:\n {text},\n")
+                #        f"type: pdf\n")
+                # print()
+
+                dictionary = {
+                    "filename": f"{filename}\n",
+                    "page": f"{page}\n",
+                    "text": f"{text}\n",
+                    "type": f"pdf\n"
+                }
+
+                with open ("data.json", "a") as outfile:
+                    json.dump(dictionary, outfile)
+                    outfile.write("\n")
+
+
+json_data = requests.get("data.json").json()
+splitter = RecursiveJsonSplitter(max_chunk_size=300)
+json_chunks = splitter.split_json(json_data = json_data )
+
